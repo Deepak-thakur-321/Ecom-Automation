0e5da7d5c1adc308ea7cdd2760ae57e8cacda45f
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2026-06-06 15:52:29 +0530

    test(navbar): enhance navbar test coverage and validations

--- tests/utils/navbar.spec.js
+++ tests/utils/navbar.spec.js
@@ -2,6 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test.describe("Navbar Tests", () => {
 
+   async function getNavbar(page) {
+      const navbar = page.getByRole("navigation")
+      await expect(navbar).toBeVisible();
+      return navbar;
+   };
+
    test.beforeEach(async ({ page }) => {
       await page.goto("/home");
       await expect(page.getByRole("navigation")).toBeVisible();
@@ -13,8 +19,7 @@ test.describe("Navbar Tests", () => {
 
 
    test("Should display all nav links", async ({ page }) => {
-      const navbar = page.getByRole("navigation")
-      await expect(navbar).toBeVisible();
+      const navbar = await getNavbar(page)
       const links = ["Best Sellers", "Gallery", "New Collection", "Sale",];
       for (const link of links) {
          await expect(navbar.getByRole("link", { name: link })).toBeVisible();
@@ -26,133 +31,161 @@ test.describe("Navbar Tests", () => {
    });
 
    test("Should display cart icon", async ({ page }) => {
-      const navbar = page.getByRole("navigation")
-      await expect(navbar).toBeVisible();
-      await expect(page.locator('a[href="/cart"]').count());
+      const navbar = await getNavbar(page)
+      await expect(navbar.locator('a[href="/cart"]').count());
    });
 
    test("Should display wishlist icon", async ({ page }) => {
-      const navbar = page.getByRole("navigation");
-      await expect(navbar).toBeVisible();
-      await expect(page.locator('a[href="/wishlist"]'))
+      const navbar = await getNavbar(page)
+      await expect(navbar.locator('a[href="/wishlist"]'))
    });
 
    test("Should display profile icon", async ({ page }) => {
-      const navbar = page.getByRole("navigation");
-      await expect(navbar).toBeVisible();
+      const navbar = await getNavbar(page)
       await expect(navbar.locator('a[href="/profile"]'))
    });
 
-   // ─── Navigation ───────────────────────────────────────
+   //  Navigation 
    test("Should navigate to home on VELOR logo click", async ({ page }) => {
-      await page.goto("/sale"); // kisi aur page pe jao pehle
+      await page.goto("/sale");
       await page.getByRole("navigation").getByText("VELOR").click();
       await expect(page).toHaveURL(/home/);
    });
 
    test("Should navigate to Best Sellers page", async ({ page }) => {
-      await page.getByRole("link", { name: "Best Sellers" }).click();
+      const navbar = await getNavbar(page)
+      await navbar.getByRole("link", { name: "Best Sellers" }).click();
       await expect(page).toHaveURL(/best-sellers/);
    });
 
    test("Should navigate to Gallery page", async ({ page }) => {
-      await page.getByRole("link", { name: "Gallery" }).click();
+      const navbar = await getNavbar(page)
+      await navbar.getByRole("link", { name: "Gallery" }).click();
       await expect(page).toHaveURL(/gallery/);
    });
 
    test("Should navigate to New Collection page", async ({ page }) => {
-      await page.getByRole("link", { name: "New Collection" }).click();
+      const navbar = await getNavbar(page)
+      await navbar.getByRole("link", { name: "New collection" }).click();
       await expect(page).toHaveURL(/view-collection/);
    });
 
    test("Should navigate to Sale page", async ({ page }) => {
-      await page.getByRole("link", { name: "Sale" }).click();
+      const navbar = await getNavbar(page)
+      await navbar.getByRole("link", { name: "Sale" }).click();
       await expect(page).toHaveURL(/sale/);
    });
 
    test("Should navigate to cart on cart icon click", async ({ page }) => {
-      await page.locator('a[href="/cart"]').click();
+      const navbar = await getNavbar(page)
+      await navbar.locator('a[href="/cart"]').click();
       await expect(page).toHaveURL(/cart/);
    });
 
    test("Should navigate to wishlist on wishlist icon click", async ({ page }) => {
-      await page.locator('a[href="/wishlist"]').click();
+      const navbar = await getNavbar(page)
+      await navbar.locator('a[href="/wishlist"]').click();
       await expect(page).toHaveURL(/wishlist/);
    });
 
    // ─── Categories Dropdown ──────────────────────────────
    test("Should open categories dropdown on hover", async ({ page }) => {
       await page.getByRole("button", { name: /categories/i }).hover();
-      // Dropdown mein T-Shirts link visible hona chahiye
-      await expect(
-         page.getByRole("link", { name: /T-Shirts/i }).first()
-      ).toBeVisible();
+      const navbar = await getNavbar(page)
+      await expect(navbar.getByRole("link", { name: /T-Shirts/i }).first()).toBeVisible();
    });
 
    test("Should navigate to category on dropdown click", async ({ page }) => {
-      await page.getByRole("button", { name: /categories/i }).hover();
-      await page.getByRole("link", { name: /T-Shirts/i }).first().click();
+      const navbar = await getNavbar(page)
+      await navbar.getByRole("button", { name: /categories/i }).hover();
+      await navbar.getByRole("link", { name: /T-Shirts/i }).first().click();
       await expect(page).toHaveURL(/t-shirts/);
    });
 
-   // ─── Search Functionality ─────────────────────────────
+   //  Search Functionality 
    test("Should show search results for valid query", async ({ page }) => {
+      const navbar = await getNavbar(page)
       const searchInput = page.getByPlaceholder("Search products...");
       await searchInput.fill("shirt");
 
-    
-      await expect(
-         page.locator('a[href*="/product/"]').first()
-      ).toBeVisible();
+      await expect(navbar.locator('a[href*="/product/"]').first()).toBeVisible();
    });
 
    test("Should show No products found for invalid query", async ({ page }) => {
       const searchInput = page.getByPlaceholder("Search products...");
       await searchInput.fill("xyzabc999");
 
-      await expect(
-         page.getByText("No products found")
-      ).toBeVisible();
+      await expect(page.getByText("No products found")).toBeVisible();
    });
 
    test("Should navigate to product on search result click", async ({ page }) => {
       const searchInput = page.getByPlaceholder("Search products...");
       await searchInput.fill("shirt");
-
-      
       await page.locator('a[href*="/product/"]').first().click();
-
-     
       await expect(page).toHaveURL(/product/);
    });
 
    test("Should clear results after clicking a result", async ({ page }) => {
       const searchInput = page.getByPlaceholder("Search products...");
       await searchInput.fill("shirt");
-
       await page.locator('a[href*="/product/"]').first().click();
 
    });
 
-   // ─── Cart Badge ───────────────────────────────────────
-   test("Should show cart badge after adding product", async ({ page }) => {
-      
+   //  Cart Badge 
+   test.only("Should increase cart badge count after adding product", async ({ page }) => {
+
+      // Login
       await page.goto("/login");
+
       await page.locator('input[type="email"]').fill("test@mystore.com");
       await page.locator('input[type="password"]').fill("test123");
       await page.getByRole("button", { name: /sign in/i }).click();
       await page.waitForURL(/home/);
 
-    
-      await page.goto("/product/1");
-      await page.getByRole("button", { name: /ADD TO CART/i }).click();
+      // Initial Badge Count
+      const navbar = await getNavbar(page);
 
+      const cartLink = navbar.locator('a[href="/cart"]').first();
+      const badge = cartLink.locator("span");
+      let initialCount = 0;
+      if (await badge.count() > 0) { initialCount = Number(await badge.textContent()) }
+
+      console.log("Initial Badge Count:", initialCount);
+
+      // Product Page
+      await page.goto("/collection/product/1");
+      await page.getByRole("button", { name: /add to cart/i }).click();
+      await page.waitForTimeout(3000);
+
+      // DEBUG 1
+      console.log("Product Added");
+
+      // Cart Check
+      await page.goto("/cart");
+      console.log("Cart URL:", page.url());
+      console.log("Cart Page Text:", await page.locator("body").textContent());
+
+      // Verify item actually exists in cart
+      await expect(page.getByText("Shopping Bag")).toBeVisible();
+
+      // Home Page
       await page.goto("/home");
-      const cartLink = page.locator('a[href="/cart"]');
-      await expect(cartLink).toBeVisible();
+      const updatedNavbar = await getNavbar(page);
+      const updatedCartLink = updatedNavbar.locator('a[href="/cart"]').first();
+      console.log("Cart Link HTML:", await updatedCartLink.innerHTML());
+      const updatedBadge = updatedCartLink.locator("span");
+      console.log("Badge Count Found:", await updatedBadge.count());
 
-      const badge = cartLink.locator('span');
-      await expect(badge).toBeVisible();
+      if (await updatedBadge.count() > 0) {
+         console.log("Updated Badge Text:", await updatedBadge.textContent());
+         await expect(updatedBadge).toBeVisible();
+
+      } else {
+         throw new Error("Badge span not found after adding product");
+
+      }
+
    });
 
 });
